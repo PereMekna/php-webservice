2f487ef0070835feaef8418e175e40cef0cff074
PereMekna <dupont.louis4@gmail.com>
2016-12-12 11:47:09 +0100

    test url rewriting

--- controllers/register-ctrl.php
+++ controllers/register-ctrl.php
@@ -4,14 +4,20 @@
    $db = $m->admin;
    $collection = $db->users;
 
+   $length = 64;
+   $token = bin2hex(openssl_random_pseudo_bytes($length));
+
    if ($_POST["password"] == $_POST["password_conf"] && $_POST["email"] == $_POST["email_conf"]) {
       $user = array( 
          "username" => $_POST["username"], 
          "password" => $_POST["password"], 
-         "email" => $_POST["email"]
+         "email" => $_POST["email"],
+         "token" => $token,
+         "tokenEnd" => time() + (7 * 24 * 60 * 60)
       );
 
       $_SESSION["username"] = $_POST["username"];
+      $_SESSION["token"] = $token;
       
       $collection->insert($user);
    }
--- views/partials/header.php
+++ views/partials/header.php
@@ -6,13 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap 101 Template</title>
+    <title>Web service</title>
 
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
     
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -61,7 +59,7 @@
        <?php 
          if (isset($_SESSION["username"])) {
        ?>
-       <ul class="nav navbar-nav"><li><p>Connecté</p></li></ul>
+       <div class="navbar-form navbar-right"><p>Bienvenue <?php echo $_SESSION["username"]; ?> Token : <input type="text" value="<?php echo $_SESSION["token"]; ?>" /></p></div>
        <?php } ?>
 
       </div>
